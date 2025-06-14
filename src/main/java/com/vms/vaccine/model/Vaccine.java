6b67c54d7140914adb24ac00743fec833437e07b
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-14 21:30:15 +0530

    Removed unused import statements.

--- src/main/java/com/vms/vaccine/model/Vaccine.java
+++ src/main/java/com/vms/vaccine/model/Vaccine.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
