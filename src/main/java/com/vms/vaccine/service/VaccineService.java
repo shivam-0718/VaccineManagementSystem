3cba2cf7c3d5293febdae1342226f8593f81e0ad
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-14 17:25:50 +0530

    Add methods for managing multiple vaccines and fetching vaccine data.

--- src/main/java/com/vms/vaccine/service/VaccineService.java
+++ src/main/java/com/vms/vaccine/service/VaccineService.java
@@ -4,6 +4,7 @@ import com.vms.vaccine.model.Vaccine;
 import com.vms.vaccine.repo.IVaccineRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class VaccineService implements IVaccineService{
@@ -16,4 +17,42 @@ public class VaccineService implements IVaccineService{
         Vaccine vac = repo.save(vaccine);
         return "Vaccine with ID " + vac.getId() + " registered successfully with name: " + vac.getVaccineName();
     }
+
+    @Override
+    public Iterable<Vaccine> registerMultipleVaccineInfo(Iterable<Vaccine> vaccines) {
+        return repo.saveAll(vaccines);
+    }
+
+    @Override
+    public Long getVaccineCount() {
+        return repo.count();
+    }
+
+    @Override
+    public String checkVaccineAvailability(Long vaccineId) {
+        Optional<Vaccine> optional = repo.findById(vaccineId);
+        if(optional.isPresent()) {
+            Vaccine vac = optional.get();
+            Integer doses = vac.getAvailableDoses();
+            if(doses > 0) {
+                return "Vaccine with ID " + vaccineId + " is available with " + doses + " doses.";
+            } else {
+                return "Vaccine with ID " + vaccineId + " is currently out of stock.";
+            }
+        } else {
+            return "Vaccine with ID " + vaccineId + " does not exist.";
+        }
+    }
+
+    @Override
+    public Iterable<Vaccine> fetchAllVaccines() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Iterable<Vaccine> fetchVaccineByIds(Iterable<Long> ids) {
+        return repo.findAllById(ids);
+    }
+
+
 }
