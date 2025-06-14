76c6e388fd51373f33163849fc7c05d4d9718af9
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-14 19:45:41 +0530

    Add methods to retrieve vaccines by cost and name in IVaccineRepo

--- src/main/java/com/vms/vaccine/repo/IVaccineRepo.java
+++ src/main/java/com/vms/vaccine/repo/IVaccineRepo.java
@@ -3,8 +3,14 @@ package com.vms.vaccine.repo;
 import com.vms.vaccine.model.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IVaccineRepo extends JpaRepository<Vaccine, Long> {
+    List<Vaccine> findByCostEquals(Double cost);
+    List<Vaccine> findByCostLessThanEquals(Double cost);
+    List<Vaccine> findByCostBetween(Double minCost, Double maxCost);
 
+    List<Vaccine> findByVaccineNameEquals(String vaccineName);
+    List<Vaccine> findByVaccineCompanyEquals(String vaccineCompany);
 }
