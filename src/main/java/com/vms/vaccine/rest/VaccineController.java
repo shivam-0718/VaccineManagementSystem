9fc4450b7cc51e38b828b1d9e60ea16e794e011e
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-01 20:02:46 +0530

    Add VaccineController for controller methods.

---
+++ src/main/java/com/vms/vaccine/rest/VaccineController.java
@@ -0,0 +1,29 @@
+package com.vms.vaccine.rest;
+
+import com.vms.vaccine.model.Vaccine;
+import com.vms.vaccine.service.VaccineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class VaccineController {
+
+    @Autowired
+    public VaccineService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerVaccine(@RequestBody Vaccine vaccine) {
+        String response = service.registerVaccineInfo(vaccine);
+        return new ResponseEntity<String>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get-vaccines")
+    public ResponseEntity<List<Vaccine>> getVaccines() {
+        List<Vaccine> vaccines = (List<Vaccine>) service.fetchAllVaccines();
+        return new ResponseEntity<>(vaccines, HttpStatus.OK);
+    }
+}
