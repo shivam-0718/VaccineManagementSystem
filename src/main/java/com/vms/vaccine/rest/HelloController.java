5f6cb4705ab033b31c9f07d26a88c1b0d9fac1ae
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-13 15:30:44 +0530

    Added OpenAPI annotations on the controllers.

--- src/main/java/com/vms/vaccine/rest/HelloController.java
+++ src/main/java/com/vms/vaccine/rest/HelloController.java
@@ -1,5 +1,6 @@
 package com.vms.vaccine.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping(value = {"/", "/home"})
+    @Operation(summary = "GET Operation", description = "This method is used to greet the user with a welcome message.")
     public String greet() {
         return "Welcome to Vaccine Management application.";
     }
--- src/main/java/com/vms/vaccine/rest/VaccineController.java
+++ src/main/java/com/vms/vaccine/rest/VaccineController.java
@@ -2,6 +2,7 @@ package com.vms.vaccine.rest;
 
 import com.vms.vaccine.model.Vaccine;
 import com.vms.vaccine.service.VaccineService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,30 +17,35 @@ public class VaccineController {
     public VaccineService service;
 
     @PostMapping("/register")
+    @Operation(summary = "POST Operation", description = "This method is used to register a new vaccine and return the response with the created status.")
     public ResponseEntity<String> registerVaccine(@RequestBody Vaccine vaccine) {
         String response = service.registerVaccineInfo(vaccine);
         return new ResponseEntity<String>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-vaccines")
+    @Operation(summary = "GET Operation", description = "This method is used to fetch all vaccines and return them to the user in a list.")
     public ResponseEntity<List<Vaccine>> getVaccines() {
         List<Vaccine> vaccines = (List<Vaccine>) service.fetchAllVaccines();
         return new ResponseEntity<>(vaccines, HttpStatus.OK);
     }
 
     @GetMapping("/vaccine-stock/{id}")
+    @Operation(summary = "GET Operation", description = "This method is used to check the availability of a vaccine by its ID as input and return the stock details.")
     public ResponseEntity<String> vaccineStock(@PathVariable Long id) {
         String stockDetails = service.checkVaccineAvailability(id);
         return new ResponseEntity<String>(stockDetails, HttpStatus.OK);
     }
 
     @GetMapping("/get-vaccine/{id}")
+    @Operation(summary = "GET Operation", description = "This method is used to fetch a vaccine by its ID and return about it in a detailed form.")
     public ResponseEntity<List<Vaccine>> getVaccineById(@PathVariable("id") Long id) {
         List<Vaccine> vac = (List<Vaccine>) service.fetchVaccineById(id);
         return new ResponseEntity<>(vac, HttpStatus.OK);
     }
 
     @PatchMapping("/update-vaccine/{id}/{newCost}")
+    @Operation(summary = "PATCH Operation", description = "This method is used to update the cost of a vaccine by its ID and return the response with the accepted status.")
     public ResponseEntity<?> updateVaccineById(@PathVariable("id") Long id, @PathVariable("newCost") Double newCost) {
         String response = service.modifyCostById(newCost, id);
         return new ResponseEntity<String>(response, HttpStatus.ACCEPTED);
